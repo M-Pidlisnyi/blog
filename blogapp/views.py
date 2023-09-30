4d8c7ca90f6b08658e12f4e970e9df632a8dc83a
M-Pidlisnyi <mpidlisnyi88@gmail.com>
2023-09-30 13:10:13 +0300

    add detail page for posts

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import DetailView
+
 # Create your views here.
 def index(req):
 
@@ -7,3 +9,10 @@ def index(req):
 
 
     return render(req, "blogapp/index.html", context={"posts": post_list})
+
+
+class PostDetailView(DetailView):
+    template_name = "blogapp/post_detail.html"
+
+    model = Post
+
