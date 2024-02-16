e5c0f4ca998f064c838f6efb41020ef442cf242c
Misha <mpidlisnyi88@gmail.com>
2024-02-16 20:15:29 +0200

    lesson 16.02

--- settings.py
+++ settings.py
@@ -3,4 +3,4 @@ import os
 PATH = os.path.dirname(__file__) + os.sep
 PATH_STATIC = PATH + "static" + os.sep
 
-SECRET_KEY = "E74572A88EB98992145667E58596A"
+SECRET_KEY = "E74572A88EB98992145667E58596A" 
