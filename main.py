3a40604601eccc763365b1d3067eb908d3eb099a
misha <mpidlisnyi88@gmail.com>
2024-03-01 20:31:26 +0200

    lesson 01.03

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,  url_for, redirect
+from flask import Flask, render_template,  url_for, redirect, request
 import settings as stg
 import db_scripts as db
 
@@ -10,9 +10,22 @@ app.config["SECRET_KEY"] = stg.SECRET_KEY
 def index():
    return render_template("index.html")
 
-@app.route("/post/category")
-def post_category():
-    return "ТУТ БУДУТЬ КАТЕГОРІЇ ПОСТІВ"
+@app.route("/post/category/<category_name>", methods=["GET", "POST"])
+def post_category(category_name):
+    category_id = db.getIdByCategory(category_name)
+
+    if request.method == "GET":
+        posts = db.getPostsByCategory(category_name)
+    
+    return render_template("post_category.html", 
+                           category_name=category_name,
+                           category_id=category_id,
+                           posts = posts) 
+
+
+
+
+
 
 @app.route("/post/view")
 def post_view():
