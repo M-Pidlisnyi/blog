501b537c67741f0f0cad8bcc82ad6d2c63b69e9e
misha <mpidlisnyi88@gmail.com>
2024-03-08 20:29:08 +0200

    lesson 08.03

--- main.py
+++ main.py
@@ -16,15 +16,16 @@ def post_category(category_name):
 
     if request.method == "GET":
         posts = db.getPostsByCategory(category_name)
+
+    if request.method == "POST":
+        db.addPost(request.form["category_id"], request.form["post"])
+        posts = db.getPostsByCategory(category_name)
+        return redirect(f"/post/category/{category_name}")
     
     return render_template("post_category.html", 
                            category_name=category_name,
                            category_id=category_id,
                            posts = posts) 
-
-
-
-
 
 
 @app.route("/post/view")
@@ -36,6 +37,11 @@ def about():
     user = db.getUser()
     print(user)
     return render_template("about.html", user=user)
+
+@app.route("/post/delete/<post_id>/<category_name>")
+def deletePost(post_id, category_name):
+    db.deletePost(post_id)
+    return redirect(f"/post/category/{category_name}")
 
 
  
